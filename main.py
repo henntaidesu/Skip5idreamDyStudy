c1e1fed6de207a8e69d5e488c8deff02d93f0d3d
henntaidesu <58099835+henntaidesu@users.noreply.github.com>
2023-10-16 22:04:16 +0800

    修复BUG

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import re
 
 print("欢迎使用盗梦空间抖音学习跳过脚本")
+print("程序不会校验你的真实姓名和学号")
+
 
 while True:
     userName = input("请输入姓名: ")
@@ -18,32 +20,40 @@ while True:
     else:
         print("学号只能是字母和数字")
 
-schoolName = input("请输入学校名称: ")
+while True:
+    flag = 0
+    if flag != 0:
+        print(userName)
+        print(studentNo)
+    flag += 1
+    schoolName = input("请输入学校名称: ")
 
-# 目标URL
-url = "https://h5.5idream.net/training/apply"
-# 要传递的数据
-data = {
-    "userName": userName,
-    "studentNo": studentNo,
-    "schoolName": schoolName,
-    "type": 8
-}
+    # 目标URL
+    url = "https://h5.5idream.net/training/apply"
+    # 要传递的数据
+    data = {
+        "userName": userName,
+        "studentNo": studentNo,
+        "schoolName": schoolName,
+        "type": 8
+    }
 
-# 发送POST请求
-response = requests.post(url, data=data)
-response_data = json.loads(response.text)
+    # 发送POST请求
+    response = requests.post(url, data=data)
+    response_data = json.loads(response.text)
 
-# 检查响应状态码
-if response.status_code == 200:
-    if "data" in response_data and response_data["data"]:
-        print("POST请求成功，成功跳过盗梦空间抖音学习")
-    if "data" in response_data and response_data["code"] == "学号只能是字母和数字":
-        print("检查学校是否正确")
+    # 检查响应状态码
+    if response.status_code == 200:
+        if "data" in response_data and response_data["data"]:
+            print("POST请求成功，成功跳过盗梦空间抖音学习")
+            print("三个工作日后登录下面网址查看证书")
+            print("https://www.5idream.net/public/activity/certificate/list.html")
+            break
+        if "data" in response_data and response_data["code"] == "学号只能是字母和数字":
+            print("检查学校是否正确")
+        else:
+            print("检查学校名称是否正确")
     else:
-        print("检查学校名称是否正确")
-else:
-    print(f"POST请求失败，状态码: {response.status_code}")
+        print(f"POST请求失败，状态码: {response.status_code}")
 
-# 可以输出响应内容以查看返回结果
 
